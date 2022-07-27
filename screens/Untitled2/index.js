c9b693b64b87c807abc0c8c273851603a3925cc7
Mithilesh12@crowdbotics.com <Mithilesh12@crowdbotics.com>
2022-07-27 21:17:01 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,26 @@
+import { Text } from "react-native";
+import React from "react";
+import { View } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={{
+    backgroundColor: '#f0f0f1',
+    padding: 10,
+    position: 'relative',
+    height: '100%'
+  }}>
+      <Text style={{
+      left: 73,
+      top: 99,
+      position: "absolute",
+      width: 100,
+      height: 50,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      color: "#c01c1c",
+      textAlign: "center"
+    }}>ckxklvcxkvcxjkvjzx</Text></View>;
+};
+
+export default Untitled2;
